71aee4f03c5dc5a0b32dace472679818e459f352
璐璐 <61314@DESKTOP-HUGRA9S>
2019-03-04 18:02:15 +0800

    健康资讯编辑页面

---
+++ MyHabits.MvcWeb/Scripts/MyScript/PublishHeal.js
@@ -0,0 +1,24 @@
+﻿$(function () {
+    Healinfo();
+});
+function Healinfo() {
+    //发布按钮点击事件
+    $("#btnRelease").click(function () {
+        var heal_title = $("#txtTitle").val();
+        var heal_content = $("#editor").html();
+        var heal_type = $("#heal_type").val();
+        healtype();
+        console.log("接下来为标题内容和类型");
+        console.log(heal_title);
+        console.log(heal_content);
+        console.log(heal_type);
+    });
+};
+function healtype() {
+    var heal_type = $("#heal_type").val();
+    if (heal_type == '0') {
+        $(".redstart").text("*文章类型必填");
+    } else {
+        $(".redstart").text("*");
+    }
+};
